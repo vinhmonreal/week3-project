e0b4f405fb090cae8b430ded5b3b1918d7dea087
vinhmonreal <129719083+vinhmonreal@users.noreply.github.com>
2023-04-29 13:20:48 -0500

    Add files via upload
    revised

--- ROI.py
+++ ROI.py
@@ -1,81 +1,78 @@
 
 class Return_on_Investment():
-    
-    income = {}
-    expenses = {}
-    cash_on_cash = {}
-    cash_flow = None
-    income_total = None
-    expenses_total = None
-    roi = None
-    cash_on_cash_total = None
+    my_dict = {
+        'income' : {},
+        'expenses' : {},
+        'cash_on_cash' : {},
+    }
 
-    def driver(self):
-        while True:
-            res_income_name = input('What is your income? or enter done when done\n').lower()
-            if res_income_name == 'done':
-                break
-            try:
-                res_income_ammount = int(input('What is the ammount?\n'))
-                self.income[res_income_name] = int(res_income_ammount)
-            except:
-                print('Please enter valid number of ammount')
-        print(self.income)
-        while True:
-            res_expenses_name = input('What is your expense? or enter done when done\n').lower()
-            if res_expenses_name == 'done':
-                break
-            try:
-                res_expenses_ammount = int(input('What is the ammount?\n'))
-                self.expenses[res_expenses_name] = int(res_expenses_ammount)
-            except:
-                print('Please enter valid number of ammount')
-        print(self.expenses)
-        while True:
-            res_cash_on_cash_name = input('What is your cash on cash expense? or enter done when done\n').lower()
-            if res_cash_on_cash_name == 'done':
-                break
-            try:
-                res_cash_on_cash_ammount = int(input('What is the ammount?\n'))
-                self.cash_on_cash[res_cash_on_cash_name] = int(res_cash_on_cash_ammount)
-            except:
-                print('Please enter valid number of ammount')
-        print(self.cash_on_cash)
-        self.get_income()
-        self.get_expenses()
-        self.get_cashflows()
-        self.get_cash_on_cash()
-        self.get_ROI()   
+    cash_flow_sum = None
+    income_sum = None
+    expenses_sum = None
+    cash_on_cash_sum = None
+    roi = None
+
+    def add_income(self, key, value):
+        try:
+            value = float(value)
+            self.my_dict['income'][key] = value
+        except:
+            print('Error...invalid value')
+            return 0
+            
     def get_income(self):
-        self.income_total = sum(self.income.values())
-        print(f'Total income: ${self.income_total }\n')
-        return self.income_total 
+        self.income_sum = sum(self.my_dict['income'].values())
+        print(f'Total income: ${self.income_sum }\n')
+        return self.income_sum 
+    
+    def add_expenses(self, key, value):
+        try:
+            value = float(value)
+            self.my_dict['expenses'][key] = value
+        except:
+            print('Error...invalid value')
+            return 0
+            
     def get_expenses(self):
-        self.expenses_total = sum(self.expenses.values())
-        print(f'Total Expenses: ${self.expenses_total}\n')
-        return self.expenses_total
+        self.expenses_sum = sum(self.my_dict['expenses'].values())
+        print(f'Total Expenses: ${self.expenses_sum}\n')
+        return self.expenses_sum
+    
     def get_cashflows(self):
-        if self.income_total != None and self.expenses_total != None:
-            self.cash_flow = self.income_total - self.expenses_total
-            print(f'Cash flow: ${self.cash_flow}\n')
-            return self.cash_flow
+        if self.income_sum != None and self.expenses_sum != None:
+            self.cash_flow_sum = sum(self.my_dict['income'].values()) - sum(self.my_dict['expenses'].values())
+            print(f'Cash flow: ${self.cash_flow_sum}\n')
+            return self.cash_flow_sum
         else:
-            message = 'Error...invalid type due to empty income or empty expense'
-            print(message)
-            return message
+            print('Error...empty incomes or empty expenses')
+            return 0
+        
+    def add_cash_on_cash(self, key, value):
+        try:
+            value = float(value)
+            self.my_dict['cash_on_cash'][key] = value
+        except:
+            print('Error...invalid value')
+            return 0
+        
     def get_cash_on_cash(self):
-        self.cash_on_cash_total = sum(self.cash_on_cash.values()) 
-        print(f'Total investment: ${self.cash_on_cash_total}\n')
-        return self.cash_on_cash_total
+        self.cash_on_cash_sum = sum(self.my_dict['cash_on_cash'].values()) 
+        print(f'Total investment: ${self.cash_on_cash_sum}\n')
+        return self.cash_on_cash_sum
+    
     def get_ROI(self):
-        if self.cash_flow != None and self.cash_on_cash_total != None and self.cash_on_cash_total != 0:
-            self.roi = self.cash_flow * 12 / self.cash_on_cash_total * 100
+        if self.my_dict['cash_on_cash'] != {} and self.my_dict['income'] != {} and self.my_dict['expenses'] != {}:
+            self.roi = round((sum(self.my_dict['income'].values()) - sum(self.my_dict['expenses'].values())) * 12 / sum(self.my_dict['cash_on_cash'].values()) * 100, 2)
             print(f'ROI: {self.roi} %\n')
             return self.roi
         else:
-            message = 'Error...Cash on Cash is zero'
-            print(message)
-            return message
+            print('Error...empty incomes or empty expenses or empty cash on cash')
+            return 0
+  
 
-# my = Return_on_Investment()
-# my.driver()
+
+
+
+
+
+
--- Tesing.py
+++ Tesing.py
@@ -4,42 +4,51 @@ from ROI import Return_on_Investment
 class Test_Return_on_Investment(unittest.TestCase):
     def test_get_income(self):
         a = Return_on_Investment()
-        a.income = {'rent' : 1000 , 'bonus' : 100}
-        self.assertEqual(sum(a.income.values()),1100)
+        a.add_income('rent' ,  1000)
+        a.add_income('bonus' , 100.8)
+        self.assertEqual(a.get_income(), 1100.8)
         b = Return_on_Investment()
-        b.income = {'rent' : 2000 , 'bonus' : 300}
-        self.assertEqual(sum(b.income.values()),2300)
+        self.assertEqual(b.add_income('rent' , 900j), 0)
     def test_get_expenses(self):
         a = Return_on_Investment()
-        a.expenses = {'water' : 100 , 'HOA' : 100, 'Mort': 700}
-        self.assertEqual(sum(a.expenses.values()),900)
+        a.add_expenses('water' , 100.70 )
+        a.add_expenses('HOA' , 100)
+        a.add_expenses('Mort', 700)
+        self.assertEqual(a.get_expenses(), 900.7)
         b = Return_on_Investment()
-        b.expenses = {'water' : 50 , 'HOA' : 90, 'Mort': 700}
-        self.assertEqual(sum(b.expenses.values()),840)
+        self.assertEqual(b.add_expenses('HOA' , 90j), 0)
     def test_get_cash_flow(self):
         a = Return_on_Investment()
-        a.income_total = 1000
-        a.expenses_total = 800
-        a.get_cashflows()
-        self.assertEqual(a.cash_flow, 200)
+        a.add_income('rent' ,  1000.50)
+        a.add_income('bonus' , 100)
+        a.add_expenses('water' , 100 )
+        a.add_expenses('HOA' , 100)
+        a.add_expenses('Mort', 700)
+        a.get_expenses()
+        a.get_income()
+        self.assertEqual(a.get_cashflows(), 200.5)
     def test_get_cash_on_cash(self):
         a = Return_on_Investment()
-        a.cash_on_cash = {'Down Payment':40000, 'Closing Cost' : 5000, 'Misc':2500}
-        self.assertEqual(sum(a.cash_on_cash.values()), 47500)
+        a.add_cash_on_cash('Down Payment',40000)
+        a.add_cash_on_cash('Closing Cost',5000)
+        a.add_cash_on_cash('Misc',2500)
+        self.assertEqual(a.get_cash_on_cash(), 47500)
+        b = Return_on_Investment()
+        self.assertEqual(b.add_cash_on_cash('Down Payment',40000j), 0)
     def test_get_ROI(self):
         a = Return_on_Investment()
-        a.cash_flow = 200
-        a.cash_on_cash_total = 50000
-        a.get_ROI()
-        self.assertEqual(a.roi, 4.8) 
-    def test_zero_cash_on_cash(self):
-        a = Return_on_Investment()
-        a.cash_flow = 200
-        self.assertEqual(a.get_ROI(), 'Error...Cash on Cash is zero') 
-        b = Return_on_Investment()
-        b.income_total = None
-        b.expenses_total = None
-        self.assertEqual(b.get_cashflows(),'Error...invalid type due to empty income or empty expense') 
+        a.add_income('rent' ,  1000)
+        a.add_income('bonus' , 100)
+        a.add_expenses('water' , 100 )
+        a.add_expenses('HOA' , 100)
+        a.add_expenses('Mort', 700)
+        a.add_cash_on_cash('Down Payment',40000)
+        a.add_cash_on_cash('Closing Cost',4000)
+        a.add_cash_on_cash('Misc',2400)
+        a.get_expenses()
+        a.get_income()
+        a.get_cash_on_cash()
+        self.assertEqual(a.get_ROI(), 5.17) 
         
         
 if __name__ == '__main__':
